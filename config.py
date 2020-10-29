f0e000c0916565bbf8602b9dd457f4b00faa052f
jbmerville <jbmer@my.yorku.ca>
2020-10-29 17:21:07 +0100

    Initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+email = "your_email@gmail.com"
+password = "your_password"
+dirToDownloadResumesTo = "path/To/Dir"
+
+# Allowed colors:
+# (please forgive my color naming abilities... I tried :D):
+colors = ["black", "red", "wine", "grape", "purple", "ocean", "blue", "sky", "cyan", "swamp",
+          "green", "grass", "vomit", "yellow", "sun", "orange", "red", "brown", "gray", "metal"]
+
+# Change this if the page you get errors like:
+# Message: no such element: Unable to locate element: {...}
+# Unit are in seconds.
+sleepTime = 0
